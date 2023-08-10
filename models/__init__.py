edad5c158166dab36ccdc18866b4d363716c1701
Princeeze <Trade2uwin@gmail.com>
2023-08-10 04:11:20 +0100

    Commented all line that contains files not created yet

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """__init__ initializes the models directory"""
-from models.engine.file_storage import FileStorage
+#from models.engine.file_storage import FileStorage
 
 
-storage = FileStorage()
-storage.reload()
+#storage = FileStorage()
+#storage.reload()
