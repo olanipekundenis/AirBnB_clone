9827b29c09c30a649bca8365be2340b80c47c17e
Princeeze <Trade2uwin@gmail.com>
2023-08-13 00:08:03 +0100

    Testing city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -11,15 +11,17 @@ from models.engine.file_storage import FileStorage
 import os
 from models import storage
 from models.base_model import BaseModel
+from tests.test_models.test_base_model import TestBaseModel
 
 
-class TestCity(unittest.TestCase):
+class TestCity(TestBaseModel):
 
     """Test Cases for the City class."""
 
     def setUp(self):
         """Sets up test methods."""
-        pass
+        self.name = "City"
+        self.value = City
 
     def tearDown(self):
         """Tears down test methods."""
@@ -40,13 +42,17 @@ class TestCity(unittest.TestCase):
         self.assertIsInstance(b, City)
         self.assertTrue(issubclass(type(b), BaseModel))
 
-    # def test_attributes(self):
-        """Tests the attributes of City class."""
-    #    attributes = storage.attributes()["City"]
-    #    o = City()
-    #    for k, v in attributes.items():
-    #        self.assertTrue(hasattr(o, k))
-    #        self.assertEqual(type(getattr(o, k, None)), v)
+    def test_state_id(self):
+        """ """
+        new = self.value()
+        self.assertTrue(hasattr(new, "state_id"))
+        self.assertEqual(new.state_id, "")
+
+    def test_name(self):
+        """ """
+        new = self.value()
+        self.assertTrue(hasattr(new, "name"))
+        self.assertEqual(new.name, "")
 
 
 if __name__ == "__main__":
